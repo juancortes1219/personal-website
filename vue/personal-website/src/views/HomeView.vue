855c9b2e97c326a0482cba9725fdd14ab0fd2c6e
juancortes1219 <juan.cortes1219@gmail.com>
2023-11-24 17:21:08 -0500

    Created a jumbotron.

--- vue/personal-website/src/views/HomeView.vue
+++ vue/personal-website/src/views/HomeView.vue
@@ -1,34 +1,31 @@
 <template>
-  <div class="home">
-    <h1>
-      A
-      <VueTyper
-        :text='["Programmer","Photographer"]'
-        :repeat='Infinity'
-        :shuffle='false'
-        initial-action='erasing'
-        :pre-type-delay='70'
-        :type-delay='70'
-        :pre-erase-delay='3000'
-        :erase-delay='70'
-        erase-style='backspace'
-        :erase-on-complete='false'
-        caret-animation='blink'
-      ></VueTyper>
-    </h1>
+  <div class="my-5 home-jumbotron">
+    <div class="p-5 text-center">
+      <div class="container py-5">
+        <h1 class="display-1 text-body-emphasis">Hello, I'm Juan Cortes.</h1>
+        <p class="h2 col-lg-8 mx-auto">
+          A
+          <VueTyper
+            :text='["Programmer","Photographer"]'
+            :repeat='Infinity'
+            :shuffle='false'
+            initial-action='erasing'
+            :pre-type-delay='70'
+            :type-delay='70'
+            :pre-erase-delay='3000'
+            :erase-delay='70'
+            erase-style='backspace'
+            :erase-on-complete='false'
+            caret-animation='blink'
+          ></VueTyper>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
 .vue-typer {
   font-weight: bold;
-}
-
-@media (min-width: 1024px) {
-  .home {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
 }
 </style>
