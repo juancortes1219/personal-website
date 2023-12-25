1072361ac6b860838e0faab17f425cb8f6b39c35
juancortes1219 <juan.cortes1219@gmail.com>
2023-12-25 01:49:14 -0500

    Reverted ContactForm.

--- vue/vue-website/src/components/ContactForm.vue
+++ vue/vue-website/src/components/ContactForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="contact-form" @submit.prevent="submitForm">
+  <form class="contact-form" @submit.prevent="sendEmail">
     <!-- Name input -->
     <MDBInput
       type="text"
@@ -38,105 +38,49 @@
       required
     />
 
-    <!-- reCAPTCHA -->
-    <div v-if="!isHuman">
-      <p class="recaptcha-fail">reCAPTCHA verification failed. Please ensure you are not a bot.</p>
-    </div>
-    <div id="recaptcha-placeholder"></div>
-
     <!-- Submit button -->
-    <MDBBtn block class="mb-4" type="submit" :disabled="!isHuman"> Send </MDBBtn>
+    <MDBBtn block class="mb-4" type="submit"> Send </MDBBtn>
   </form>
 </template>
 
 <script setup lang="ts">
 import { MDBInput, MDBBtn, MDBTextarea } from 'mdb-vue-ui-kit'
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 import emailjs from '@emailjs/browser'
 
 const form4Name = ref('')
 const form4Email = ref('')
 const form4Subject = ref('')
 const form4Textarea = ref('')
-const isHuman = ref(false)
 
-const RECAPTCHA_SITE_KEY = '6LefgTspAAAAAP-MMSKwQJipzhr_dEdtJTzaAHUm'
-const EMAILJS_SERVICE_ID = 'service_gdszpy1'
-const EMAILJS_TEMPLATE_ID = 'template_czq9b2t'
-const EMAILJS_USER_ID = 'HijV1CR_uVeS_0V-8'
-
-declare const grecaptcha: {
-  ready(callback: () => void): void
-  execute(siteKey: string, options: { action: string }): Promise<{ score: number }>
-}
-
-// Calls reCAPTCHA v3 when the component is mounted
-onMounted(() => {
+const sendEmail = async () => {
   try {
-    const script = document.createElement('script')
-    script.src = `https://www.google.com/recaptcha/api.js?render=${RECAPTCHA_SITE_KEY}`
-    script.async = true
-    script.defer = true
-
-    script.onload = () => {
-      grecaptcha.ready(() => {
-        // Sets up reCAPTCHA callback
-        grecaptcha
-          .execute(RECAPTCHA_SITE_KEY, { action: 'homepage' })
-          .then((recaptchaResponse) => {
-            console.log('reCAPTCHA Response:', recaptchaResponse)
-
-            // Sets isHuman based on desired score threshold
-            isHuman.value = recaptchaResponse.score > 0.5
-          })
-          .catch((error) => {
-            console.error('Error fetching reCAPTCHA response:', error)
-          })
-      })
+    const templateParams = {
+      to_name: 'Juan',
+      from_name: form4Name.value,
+      from_email: form4Email.value,
+      subject: form4Subject.value,
+      message: form4Textarea.value
     }
 
-    document.head.appendChild(script)
+    const response = await emailjs.send(
+      'service_gdszpy1',
+      'template_czq9b2t',
+      templateParams,
+      'HijV1CR_uVeS_0V-8'
+    )
+
+    console.log('Email sent successfully:', response)
+
+    // Reset form fields after successful submission
+    form4Name.value = ''
+    form4Email.value = ''
+    form4Subject.value = ''
+    form4Textarea.value = ''
   } catch (error) {
-    console.error('Error loading reCAPTCHA script:', error)
-  }
-})
-
-/* Submits form */
-const submitForm = async () => {
-  if (isHuman.value) {
-    // Tries to send email
-    try {
-      const templateParams = {
-        to_name: 'Juan',
-        from_name: form4Name.value,
-        from_email: form4Email.value,
-        subject: form4Subject.value,
-        message: form4Textarea.value
-      }
-
-      const response = await emailjs.send(
-        EMAILJS_SERVICE_ID,
-        EMAILJS_TEMPLATE_ID,
-        templateParams,
-        EMAILJS_USER_ID
-      )
-
-      console.log('Email sent successfully:', response)
-
-      // Reset form fields after successful submission
-      form4Name.value = ''
-      form4Email.value = ''
-      form4Subject.value = ''
-      form4Textarea.value = ''
-    } catch (error) {
-      console.error('Error sending email:', error)
-    }
-  } else {
-    // Handles the case where reCAPTCHA v3 score is not high enough
-    console.error('reCAPTCHA v3 score is too low. Please verify that you are not a bot.')
+    console.error('Error sending email:', error)
   }
 }
-/* Submits form */
 </script>
 
 <style scoped>
@@ -149,13 +93,6 @@ const submitForm = async () => {
 }
 .btn:hover {
   background-color: var(--accent-three);
-}
-
-.recaptcha-fail {
-  padding: 0.5rem;
-  color: white;
-  background-color: rgba(255, 0, 0, 0.5);
-  border-radius: 0.5rem;
 }
 
 @media (min-width: 992px) {
